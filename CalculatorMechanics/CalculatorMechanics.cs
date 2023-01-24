76dffe9ccfdc8745edec31412bd08b40cfb09922
SergCherkasov <serg.cherkasov@gmail.com>
2023-01-24 15:30:29 +0200

    Created GetPriority method

--- CalculatorMechanics/CalculatorMechanics.cs
+++ CalculatorMechanics/CalculatorMechanics.cs
@@ -8,5 +8,19 @@ namespace Calculator.CalculatorMechanics
 {
     internal class CalculatorMechanics
     {
+        private byte GetPriority(char c)
+        {
+            switch (c)
+            {
+                case '(': return 0;
+                case ')': return 1;
+                case '+': return 2;
+                case '-': return 3;
+                case '*': return 4;
+                case '/': return 5;
+
+                default: return 6;
+            }
+        }
     }
 }
